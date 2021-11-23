8bf85ce061f1812837448e552e68fab1508ee1a9
Ezequiel <ezee.-96@gmail.com>
2021-11-22 23:18:55 -0300

    clear button

--- index.js
+++ index.js
@@ -5,11 +5,18 @@ function makeRows(rows, cols) {
   container.style.setProperty('--grid-cols', cols);
   for (c = 0; c < (rows * cols); c++) {
     let cell = document.createElement("div");
-    cell.addEventListener('click',(e)=>{
+    cell.addEventListener('mouseover',(e)=>{
         e.target.style.background="black"
     })
     container.appendChild(cell).className = "grid-item";
   };
 };
 
+const clearButton = document.getElementById("buttonClear");
+clearButton.addEventListener('click', () => {
+  cuadrados = document.querySelectorAll(".grid-item");
+  for (let i = 0; i < cuadrados.length; i++) {
+    cuadrados[i].style.background = "white";}
+});
+    
 makeRows(16, 16);
